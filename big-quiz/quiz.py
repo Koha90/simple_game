33cf9864bdac7cfde8fe04824c4d8a19181f8165
Koha90 <aleksey.jake@gmail.com>
2020-05-12 22:37:13 +0500

    Test

--- big-quiz/quiz.py
+++ big-quiz/quiz.py
@@ -14,9 +14,23 @@ main_box.move_ip(50, 40)
 timer_box.move_ip(990, 40)
 answer_box1.move_ip(50, 358)
 answer_box2.move_ip(735, 358)
-answer_box3.move_ip(50, 358)
-answer_box4.move_ip(735, 358)
+answer_box3.move_ip(50, 538)
+answer_box4.move_ip(735, 538)
 answer_boxes = [answer_box1, answer_box2, answer_box3, answer_box4]
+
+score = 0
+time_left = 10
+
+q1 = ['What is the capital of France?',
+      'London', 'Paris', 'Berlin', 'Tokyo', 2]
+q2 = ["What is 5+7?",
+      "12", "10", "14", "8", 1]
+q3 = ["What is the seventh month of the year?",
+      "April", "May", "June", "July", 4]
+q4 = ["Which planet is closest to the Sun?",
+      "Saturn", "Neptune", "Mercury", "Venus", 3]
+q5 = ["Where are the pyramids?",
+      "India", "Egypt", "Morocco", "Canada", 2]
 
 
 def draw():

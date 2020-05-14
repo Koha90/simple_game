879e8b415a943f56bb7410de2634244a3ee41fcc
Koha90 <aleksey.jake@gmail.com>
2020-05-14 19:48:03 +0500

    Test

---
+++ balloon-flight/baloon.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+WIDTH = 800
+HEIGHT = 600
+
+ballooon = Actor('balloon')
+balloon.pos = 400, 300
+
+bird = Actor("bird-up")
+bird.pos = randint(800, 1600), randint(10, 200)
+
+house = Actor("house")
+house.pos = randint(800, 1600), 460
+
+tree = Actor("tree")
+tree.pos = randint(800, 1600), 450

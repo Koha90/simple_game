f8c3cb266c50c36c86568369be5c2208b6c538cb
Koha90 <aleksey.jake@gmail.com>
2020-05-19 10:19:03 +0500

    Game Baloon

--- balloon-flight/baloon.py
+++ balloon-flight/baloon.py
@@ -15,3 +15,11 @@ house.pos = randint(800, 1600), 460
 
 tree = Actor("tree")
 tree.pos = randint(800, 1600), 450
+
+bird_up = True
+up = False
+game_over = False
+score = 0
+number_of_updates = 0
+
+score = []
